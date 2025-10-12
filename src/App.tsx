082bc78397c0db0653d80ddc9b8a78183327852d
Quentin Smith <quentin@shockedfuture.onmicrosoft.com>
2025-10-11 21:22:11 -0500

    Add Vercel Analytics & Add BWT Auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { About } from './components/About';
 import { Games } from './components/Games';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 
 export default function App() {
