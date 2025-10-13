11f9f4044cc978b21724ce69d72fc1fcdd741ea2
Quentin Smith <quentin@shockedfuturestudios.com>
2025-10-12 19:56:36 -0500

    remove beta alert

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,8 +13,9 @@ export function Hero() {
         <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-gray/10 rounded-full blur-3xl animate-pulse delay-1000" />
       </div>
 
-      {/* [START] quick alert tooltip */}
+
       <div className="relative z-10 container mx-auto px-6 text-center">
+        {/* [START] quick alert tooltip }
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
