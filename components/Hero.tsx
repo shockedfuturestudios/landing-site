7323c3a6ddfffb0abd62f6471a2915a37345d8a9
Quentin Smith <quentin@shockedfuturestudios.com>
2025-10-22 23:44:27 -0500

    Add Patreon & Copyright acknowledgement

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,11 +17,11 @@ export function HeroSection() {
           <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-16 py-5 text-sm font-medium text-white backdrop-blur-3xl">
-              <Link href="#games">Explore our Games</Link>
+              <Link href="https://patreon.com/ShockedFutureStudios">Support Us on Patreon</Link>
             </span>
           </button>
           <button className="inline-flex h-12 font-semibold animate-shimmer items-center justify-center rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-16 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            <Link href="#contact">Contact Us</Link>
+            <Link href="#games">Explore our Games</Link>
           </button>
         </div>
       </div>
